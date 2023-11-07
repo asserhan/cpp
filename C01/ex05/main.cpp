725dc335346afde536d36766d5a0e8a0a7e92a54
asserhan <asseraouhanane@gmail.com>
2023-11-07 02:40:33 +0100

    complete ex5

---
+++ C01/ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 15:58:20 by hasserao          #+#    #+#             */
+/*   Updated: 2023/11/06 23:43:30 by hasserao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    harl.complain("gdfgd");
+    harl.complain("info");
+    harl.complain("warning");
+    harl.complain("error");
+    return 0;
+}
