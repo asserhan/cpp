a95af184e9ac1bb8ff82da5966fd497eb45b87de
asserhan <asseraouhanane@gmail.com>
2023-11-07 19:59:36 +0100

    er

--- C01/ex05/Harl.cpp
+++ C01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:58:15 by hasserao          #+#    #+#             */
-/*   Updated: 2023/11/07 19:58:44 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/11/07 19:59:25 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ void Harl ::complain(std::string level)
 {
 	ptrfunc ptrf[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	std::string levels[4] = {"debug", "info", "warning", "error"};
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 4
+    ; i++)
 	{
 		if (levels[i] == level)
 			(this->*ptrf[i])();
