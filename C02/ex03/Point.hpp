070c1e0def8c1478c707b0264254a06759b5a116
asserhan <asseraouhanane@gmail.com>
2023-11-19 23:54:30 +0100

    fix const prblem in copy assignemnt operator

--- C02/ex03/Point.hpp
+++ C02/ex03/Point.hpp
@@ -4,15 +4,21 @@
 #include "Fixed.hpp"
 class Point{
     private:
-        Fixed const x;
-        Fixed const y;
+        const Fixed x;
+        const Fixed y;
     public:
     //default constructor
     Point();
     //parametrised constructor
     Point(const float x1,const float y1);
+    //copy constructor
+    Point(const Point &oobj);
+    //copy assignment operator
+    Point &operator=(const Point &oobj);
     //default destructor
     ~Point();
+    Fixed getX()const;
+    Fixed getY()const;
 
 };
 #endif
