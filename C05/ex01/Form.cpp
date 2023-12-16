e28ea84a780f7ccb3a006a1327c1218f1e0b0fad
asserhan <asseraouhanane@gmail.com>
2023-12-16 16:43:44 +0100

    implement  getters and consctuctors

---
+++ C05/ex01/Form.cpp
@@ -0,0 +1,35 @@
+#include "Form.hpp"
+Form::Form() : _name("default"), _signed(false), _gradeSign(150), _gradeExec(150) {
+    //std::cout << "Form default constructor called" << std::endl;
+}
+Form:: Form(std::string name, const int gradeToSign, const int gradeToExecute) : _name(name),_gradeExec(gradeToExecute),_gradeSign(gradeToSign){
+    //std::cout << "Form constructor called" << std::endl;
+    if(gradeToSign < 1 || gradeToExecute < 1)
+        throw Form:: GradeTooHighExeption();
+    else if(gradeToSign > 150 || gradeToExecute > 150)
+        throw Form :: GradeTooLowExeption();
+    else 
+        _signed = false;
+}
+
+Form :: Form(const Form &oobj):_gradeExec(oobj._gradeExec),_gradeSign(oobj._gradeSign){
+    std::cout << "Form copy constructor called" << std::endl;
+    (*this) = oobj;
+}
+Form &Form :: operator=(const Form &oobj){
+    std::cout << "Form copy assignment operator" << std::endl;
+    this->_signed = oobj.getSigned();
+    return(*this);
+}
+const std::string Form :: getName()const{
+    return(this->_name);
+}
+const int Form :: getGradeToSign()const{
+    return(this->_gradeSign);
+}
+const int Form :: getGradeToExecute()const{
+    return(this->_gradeExec);
+}
+bool Form :: getSigned()const{
+    return(this->_signed);
+}
---
+++ C05/ex01/Form.hpp
@@ -0,0 +1,37 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+class Form{
+    private:
+        const std::string _name;
+        bool _signed;
+        const int _gradeSign;
+        const int _gradeExec;
+    public:
+    Form();
+    Form(std::string name, const int gradeToSign, const int gradeToExecute);
+    Form(const Form &oobj);
+    Form &operator=(const Form &oobj);
+    ~Form();
+    //getters
+    const std::string getName()const;
+    bool getSigned()const;
+    const int getGradeToSign()const;
+    const int getGradeToExecute()const;
+
+    //Exception classes
+    class GradeTooHighExeption : public std::exception{
+        public:
+        virtual const char * what() const throw(){
+            return "Grade too Hight";
+        }
+    };
+    class GradeTooLowExeption : public std::exception{
+        public:
+        virtual const char * what() const throw(){
+            return "Grade Too low";
+        }
+    };
+
+};
+#endif
