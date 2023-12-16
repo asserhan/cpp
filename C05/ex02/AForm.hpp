06e2db21e1d186d3fb06c87144afb3564e42f6ff
asserhan <asseraouhanane@gmail.com>
2023-12-16 19:20:10 +0100

    update form class

---
+++ C05/ex02/AForm.hpp
@@ -0,0 +1,41 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+#include "Bureaucrat.hpp"
+class Bureaucrat;
+class Form{
+    private:
+        const std::string _name;
+        bool _signed;
+        const int _gradeSign;
+        const int _gradeExec;
+    public:
+    Form();
+    Form(std::string name, const int gradeToSign, const int gradeToExecute);
+    Form(const Form &oobj);
+    Form &operator=(const Form &oobj);
+    ~Form();
+    //getters
+    const std::string getName()const;
+    bool getSigned()const;
+    int getGradeToSign()const;
+    int getGradeToExecute()const;
+
+    //Exception classes
+    class GradeTooHighExeption : public std::exception{
+        public:
+        virtual const char * what() const throw(){
+            return "Grade too Hight";
+        }
+    };
+    class GradeTooLowExeption : public std::exception{
+        public:
+        virtual const char * what() const throw(){
+            return "Grade Too low";
+        }
+    };
+    void beSigned(Bureaucrat &bureaucrat);
+
+};
+std :: ostream &operator<<(std :: ostream &os,const Form &oobj);
+#endif
