6a455578ea85184bc5994ca763d489959b2012c3
asserhan <asseraouhanane@gmail.com>
2023-12-20 22:05:23 +0100

    fix leaks

--- C05/ex03/main.cpp
+++ C05/ex03/main.cpp
@@ -12,23 +12,19 @@ int main(int ac, char **av)
     (void)av;
     atexit(ff);
     if(ac == 1){
-                Form* rrf = NULL;
+                Form* rrf =NULL;
             try{
                 Intern someRandomIntern;
                 rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+                // Bureaucrat b("Bureaucrat", 1);
+                // rrf->beSigned(b);
+                // rrf->execute(b);
                 std::cout << *rrf << std::endl;
             }
             catch(std::exception &e){
                 std::cout << e.what() << std::endl;
             }
                 delete rrf;
-
-            // Bureaucrat b("Bureaucrat", 146);
-            // Intern i;
-            // Form *form[3];
-            // form[0] = i.makeForm("shrubbery creation", "home");
-            // form[1] = i.makeForm("robotomy request", "home");
-            // form[2] = i.makeForm("presidential pardon", "home");
 
     }else{
         std::cout << "Wrong arguments" << std::endl;
