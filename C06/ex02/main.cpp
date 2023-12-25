e4e9b383315eb75f243d324c4fe99a7755ee6e51
Hanane asseraou <asseraouhanane@gmail.com>
2023-12-25 16:51:37 +0100

    delete leaks

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -3,12 +3,9 @@
 #include "B.hpp"
 #include "C.hpp"
 
-void ff(){
-    system("leaks ex02");
-}
+
 int main(int ac,char **av){
     (void)av;
-    atexit(ff);
     if(ac == 1){
         Base *ptr = generate();
         identify(ptr);
