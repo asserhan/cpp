1460ef0e529f8645de465f5c1e3c011ea3c1b7ef
Hanane asseraou <asseraouhanane@gmail.com>
2023-12-27 17:52:59 +0100

    throw exception

--- C07/ex02/Array.hpp
+++ C07/ex02/Array.hpp
@@ -6,13 +6,19 @@ template<class T>
 class Array{
     private:
         T *tab;
-        unsigned int size;
+        unsigned int _size;
     public:
     Array();
     Array(unsigned int n);
     Array(const T &oobj);
     T & operator=(const T &oobj);
     ~Array();
+    unsigned int size() const;
+    T & operator[](unsigned int index);
+    class OutOfBoundsException : public std::exception{
+        public :
+        virtual const char * what()const throw();
+    };
 };
 
 #endif
--- C07/ex02/main.cpp
+++ C07/ex02/main.cpp
@@ -1,12 +1,12 @@
 #include "Array.hpp"
 
 template<class T>
-Array<T>:: Array() : tab(NULL),size(0){
+Array<T>:: Array() : tab(NULL),_size(0){
     std::cout << "Default constructor called" << std::endl;
 }
 
 template<class T>
-Array<T>:: Array(unsigned int n) : tab(new T[n],size(n)){
+Array<T>:: Array(unsigned int n) : tab(new T[n]),_size(n){
         std::cout << "Array created" << std::endl;
 //     for(unsigned int i = 0;i<n;i++){
 //         this->tab[i] = T();
@@ -17,18 +17,35 @@ template<class T>
 Array<T>:: ~Array(){
     std::cout << "Default destructor called" << std::endl;
 }
+
 template<class T> 
-Array<T>::Array(const T&oobj){
+Array<T>::Array(const T&oobj):tab(new T[oobj.size()]),_size(oobj.size()){
     std::cout <<"Copy constructor called"<<std::endl;
-    (*this)=oobj;
 }
+
 template<class T>
 T& Array<T> :: operator=(const T&oobj){
     std::cout <<"Copy assignment operator called"<<std::endl;
     delete[]this->tab;
-    this->tab = oobj.tab;
+    this->tab = new T[oobj.size()];
+    this->_size = oobj.size();
+    return(*this);
 } 
 
+template<class T>
+unsigned int Array<T> :: size()const {
+    return(this->_size);
+}
+
+template<class T>
+const char * Array<T> :: OutOfBoundsException :: what()const throw(){
+    return "Index out of bounds";
+}
+template<class T>
+T & Array<T> :: operator[](unsigned int index){
+    if(index > size() || index < 0)
+        throw Array::OutOfBoundsException();
+}
 int main(){
     Array<int>tab(5);
     // for(int i=0;i<5;i++){
