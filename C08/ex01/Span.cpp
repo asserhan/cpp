7bc2e88420243403ce25f85313d99abbd38bf39a
Hanane asseraou <asseraouhanane@gmail.com>
2024-01-10 12:37:21 +0100

    long and short span

--- C08/ex01/Span.cpp
+++ C08/ex01/Span.cpp
@@ -1,9 +1,13 @@
 #include "Span.hpp"
+
 Span::Span(){
     std::cout<<"default constructor called"<<std::endl;
 }
 Span::Span(unsigned int N):_N(N){
     std::cout<<"Constructor called"<<std::endl;
+    if(N==0)
+        throw std::exception();
+    this->_v.reserve(N);
 }
 Span ::Span(const Span &oobj){
     std::cout<<"copy constructor called"<<std::endl;
@@ -15,9 +19,37 @@ Span & Span:: operator=(const Span &oobj){
     return(*this);
 }
 Span :: ~Span(){
-    
+    std::cout<<"default destructor called"<<std::endl;
 }
-
 unsigned int Span :: getN() const{
     return(this->_N);
+}
+void Span :: addNumber(int n){
+    if(this->_v.size() < this->_N)
+        this->_v.push_back(n);
+    else
+        throw std::exception();
+}
+int Span :: shortestSpan(){
+    if(this->_v.size() < 2)
+        throw std::exception();
+    std::vector<int> tmp(this->_v);
+    std::sort(tmp.begin(),tmp.end());
+    int minSpan=std::numeric_limits<int>::max();
+    for(unsigned int i=0;i<tmp.size()-1;i++){
+        if(tmp[i + 1] - tmp[i] < minSpan)
+            minSpan=tmp[i + 1] - tmp[i];
+    }
+    return(minSpan);
+}
+int Span:: longestSpan(){
+    if (this->_v.size() < 2)
+        throw std::exception();
+    int min = *std::min_element(this->_v.begin(), this->_v.end());
+    int max = *std::max_element(this->_v.begin(), this->_v.end());
+    return(max - min);
+}
+void Span:: fillSpan(int start, int end){
+    while(start <= end)
+        addNumber
 }
--- C08/ex01/Span.hpp
+++ C08/ex01/Span.hpp
@@ -2,17 +2,24 @@
 #define SPAN_HPP
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <exception>
+#include <limits>
 
 class Span{
     private:
     unsigned int _N;
+    std::vector<int> _v;
     public:
     Span();
     Span(unsigned int N);
     Span(const Span &oobj);
     Span &operator=(const Span &oobj);
     ~Span();
-    void addNumber(int number);
     unsigned int  getN()const;
+    void addNumber(int n);
+    int shortestSpan();
+    int longestSpan();
+    void fillSpan(int start, int end);
 };
 #endif
