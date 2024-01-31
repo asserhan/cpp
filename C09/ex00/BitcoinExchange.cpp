bfdb2264995e39147f88a116ce4e06376bc006cc
Hanane asseraou <asseraouhanane@gmail.com>
2024-01-31 16:06:34 +0100

    read from data

--- C09/ex00/BitcoinExchange.cpp
+++ C09/ex00/BitcoinExchange.cpp
@@ -2,14 +2,26 @@
 
 BitcoinExchange :: BitcoinExchange(){
     std::ifstream file("data.csv");
-    if(!file.is_open() || is_empty(file)){
+    if(!file.is_open() ){
         std::cout<<"Error : could not open file."<<std::endl;
         exit(1);
     }
     else{
         std::string line;
+        std::getline(file,line);
+        while(std::getline(file,line)){
+            std::string key = line.substr(0,line.find(','));
+            std::string value_str = line.substr(line.find(',')+1);
+            float value = std::stof(value_str);
+            this->data[key] = value;
+            std::cout<<key<<" "<<value<<std::endl;
+        }
         
+        file.close();  
     }
+    // for(std::map<std::string,float>::iterator it = this->data.begin(); it != this->data.end(); it++){
+    //     std::cout<<it->first<<" "<<it->second<<std::endl;
+    // }
 }
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &oobj){
@@ -18,6 +30,10 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange &oobj){
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange &oobj){
     this->data = oobj.data;
     return(*this);
+}
+
+BitcoinExchange::~BitcoinExchange(){
+    
 }
 bool is_empty(std::ifstream &file){
     return(file.peek() == std::ifstream::traits_type::eof());
--- C09/ex00/main.cpp
+++ C09/ex00/main.cpp
@@ -2,18 +2,21 @@
 #include <fstream>
 
 int main(int ac,char **av){
-
-   if(ac > 2){
-        std::cout<<"Error : Wrong argument"<<std::endl;
-        return(1);
-    }
-    std::ifstream file(av[1]);
-    if(!file.is_open())
-        std::cout<<"Error : could not open file."<<std::endl;
-    else{
+(void)ac;
+(void)av;
+//    if(ac > 2){
+//         std::cout<<"Error : Wrong argument"<<std::endl;
+//         return(1);
+//     }
+//     std::ifstream file(av[1]);
+//     if(!file.is_open())
+//         std::cout<<"Error : could not open file."<<std::endl;
+//     else{
+    try{
         BitcoinExchange exchange;
-        exchange.read(file);
-        exchange.print();
+    }
+    catch(std::exception &e){
+        std::cout<<e.what()<<std::endl;
     }
      return(0);
     
